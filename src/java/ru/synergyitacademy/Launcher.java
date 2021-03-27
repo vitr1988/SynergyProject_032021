5a795134bda717d1a29e122c4533f27759384101
v.ivanov <vivanov@vtb.ru>
2021-03-27 20:11:16 +0400

    feat: Добавление первой программы

---
+++ src/java/ru/synergyitacademy/Launcher.java
@@ -0,0 +1,16 @@
+package ru.synergyitacademy;
+
+public class Launcher {
+
+    /**
+     * Основная точка входа в приложение
+     *
+     * @param args список аргументов, которые можно кастомизировать
+     * @throws Exception список исключений, которые генерируются во время выполнения программы
+     */
+    public static void main(String[] args) throws Exception {
+        //TODO: переписать данный код
+        // Выведется в консоль фраза Привет, мир!
+        System.out.println("Приветствую слушателей из набора 03.2021!");
+    }
+}
