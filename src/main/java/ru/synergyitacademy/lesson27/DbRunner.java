f4d536d068263b7593d41a6b55c3ecc3d807a3cf
v.ivanov <vivanov@vtb.ru>
2021-09-09 22:18:19 +0400

    add: JDBC API

---
+++ src/main/java/ru/synergyitacademy/lesson27/DbRunner.java
@@ -0,0 +1,26 @@
+package ru.synergyitacademy.lesson27;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DbRunner {
+
+    public static void main(String[] args) throws SQLException {
+        try (final Connection connection =
+                     DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "password");
+             final PreparedStatement statement = connection.prepareStatement("select e.emp_id, e.emp_name, e.salary, d.name department_name from employee e " +
+                     "left join department d on e.department_id = d.id");
+             final ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                System.out.println(String.format("ID: %d Name: %s Salary: %f DepartmentName: %s",
+                        resultSet.getInt("emp_id"),
+                        resultSet.getString("emp_name"),
+                        resultSet.getBigDecimal("salary"),
+                        resultSet.getString("department_name")));
+            }
+        }
+    }
+}
