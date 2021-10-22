3495a7995851019807607d5901ce6bf38d5c1181
v.ivanov <vivanov@vtb.ru>
2021-10-22 20:49:45 +0400

    add pull request

--- src/main/java/ru/synergyitacademy/lesson33/SpringBootRunner.java
+++ src/main/java/ru/synergyitacademy/lesson33/SpringBootRunner.java
@@ -3,15 +3,24 @@ package ru.synergyitacademy.lesson33;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import ru.synergyitacademy.lesson33.service.SelfInjectedService;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SpringBootRunner {
 
     public static void main(String[] args) {
+//        SpringApplication.run(SpringBootRunner.class, args);
         final ConfigurableApplicationContext context =
                 SpringApplication.run(SpringBootRunner.class, args);
         final SelfInjectedService bean = context.getBean(SelfInjectedService.class);
         bean.printApplication();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return null;
     }
 }
